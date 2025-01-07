a31f999b2566fad5e5d6727f0100d826a34540ee
Ashton(tripple blocked, owns illendo) <162846629+YeesterPlus@users.noreply.github.com>
2025-01-07 23:38:17 +0000

    FunctionSupport

--- src/nativetypes.js
+++ src/nativetypes.js
@@ -131,7 +131,7 @@ AsyncSnapFunction.prototype.init = function(context){
             };
             proc.runStep = function(...args){
                 Process.prototype.runStep.call(this,...args);
-                if ((!this.isDead )|| returned) {return}
+                if ((!((!proc.isRunning() && !proc.errorFlag) || proc.isDead) )|| returned) {return}
                 resolve(null)
             }
             stage.threads.processes.push(proc);
