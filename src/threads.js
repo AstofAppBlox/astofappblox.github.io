b19c3e059eb3c52c8835ec621f1fd5512e69de2a
Ashton(tripple blocked, owns illendo) <162846629+YeesterPlus@users.noreply.github.com>
2025-01-08 20:22:27 +0000

    ProcKeep

--- src/threads.js
+++ src/threads.js
@@ -435,58 +435,8 @@ ThreadManager.prototype.removeTerminatedProcesses = function () {
     });
     this.allprocesses = remaining;
     this.processes.forEach(proc => {
-        var result,
-            glow;
-        if ((!proc.isRunning() && !proc.errorFlag) || proc.isDead) {
-            if (proc.topBlock instanceof BlockMorph) {
-                proc.unflash();
-                // adjust the thread count indicator, if any
-                count = this.processesForBlock(proc.topBlock).length;
-                if (count) {
-                    glow = proc.topBlock.getHighlight() ||
-                        proc.topBlock.addHighlight();
-                    glow.threadCount = count;
-                    glow.updateReadout();
-                } else {
-                    proc.topBlock.removeHighlight();
-                }
-            }
-            if (proc.prompter) {
-                proc.prompter.destroy();
-                if (proc.homeContext.receiver.stopTalking) {
-                    proc.homeContext.receiver.stopTalking();
-                }
-            }
-            if (proc.topBlock instanceof ReporterBlockMorph ||
-                    proc.isShowingResult || proc.exportResult) {
-                result = proc.homeContext.inputs[0];
-                if (proc.onComplete instanceof Function) {
-                    proc.onComplete(result);
-                } else {
-                    if (result instanceof List) {
-                        proc.topBlock.showBubble(
-                            result.isTable() ?
-                                    new TableFrameMorph(
-                                        new TableMorph(result, 10)
-                                    )
-                                    : new ListWatcherMorph(result),
-                            proc.exportResult,
-                            proc.receiver
-                        );
-                    } else {
-                        proc.topBlock.showBubble(
-                            result,
-                            proc.exportResult,
-                            proc.receiver
-                        );
-                    }
-                }
-            } else if (proc.onComplete instanceof Function) {
-                proc.onComplete();
-            }
-        } else {
-            remaining.push(proc);
-        }
+        if ((!proc.isRunning() && !proc.errorFlag) || proc.isDead) return;
+        remaining.push(proc);
     });
     this.processes = remaining;
 };
